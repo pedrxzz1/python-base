8cb576a5ae615cbe6146884233c5bc9ccef406b4
pedrxzz1 <sorrisophflu@gmail.com>
2025-08-18 13:11:03 -0400

    teste

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 #!usr/bin/env python3
 """Meu primeiro Hello world
+
+exercicio com python proximo passo adicionar um leitor de ambiente
+atraves do import os
 """
 __version__= "0.1.0"
 __author__= "Pedro Henrique"
+__license__= "unlicense"
 
 print("Hello world")
