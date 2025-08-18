3ad84fadbac450ecf5c0536f278d85bedca646c4
pedrxzz1 <sorrisophflu@gmail.com>
2025-08-18 14:04:56 -0400

    adicionei um modulo

--- hello.py
+++ hello.py
@@ -1,11 +1,34 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """Meu primeiro Hello world
 
-exercicio com python proximo passo adicionar um leitor de ambiente
-atraves do import os
+Este codigo ele manda mensagem de Hello world de acordo com a linguem
+que estiver configurada no ambiente da maquina, atraves de um modulo
+padrao chamado OS.
+
+explicando como fiz isso:
+    criei uma variavel current_lenguage, importei um modulo padrao OS
+    utilizei uma funcao do mudolo os.getenv("LANG"), com essa funçao
+    ele me retorna a lingua configurada no ENV.
+
+    depois criei uma codiçao para colocar as linguas e a mensagem.
 """
 __version__= "0.1.0"
 __author__= "Pedro Henrique"
 __license__= "unlicense"
 
-print("Hello world")
+import os
+
+current_lenguage = os.getenv("LANG")[:5]
+
+if current_lenguage == "pt_BR":
+    msg = "Ola mundo!"
+elif current_lenguage == "en_US":
+    msg = "Hello world!"
+elif current_lenguage == "es_ES":
+    msg = "¡Hola, mundo!"
+elif current_lenguage == "zh_CN":
+    msg = "你好，世界"
+elif current_lenguage == "ar_SA":
+    msg = "مرحبا بالعالم"
+
+print(msg)
