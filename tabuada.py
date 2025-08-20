3b699264b5031103fc8d61d46fffca1504933292
pedrxzz1 <sorrisophflu@gmail.com>
2025-08-20 14:47:51 -0400

    atualizei o codigo

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 """Tabuada simples
+Primeira versao simples
+
+Proximo passo: 
+    adicionarei o .format para centralizar os numeros e deixar
+o programa agradavel visualmente.
+
 """
 __version__ = "0.0.1"
 __author__ = "Pedro Henrique"
@@ -8,7 +14,10 @@ __license__ = "unlicense"
 numeros = list(range(1,11))
 
 for n1 in numeros:
-    print("Tabuada do numero:", n1)
+    print("{:-^20}".format(f"Tabuada do {n1}"))
+    print()
     for n2 in numeros:
-        print(n1*n2)
-    print(25*"-")
+        res = n1*n2
+        print("{:^20}".format(f"{n1} x {n2} = {res}"))
+    print()
+    print("#"*20)
