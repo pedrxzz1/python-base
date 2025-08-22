06cb88af08901a3d8b9dd03e6050052683fc18eb
pedrxzz1 <sorrisophflu@gmail.com>
2025-08-22 16:14:31 -0400

    utilizei IO

--- hello.py
+++ hello.py
@@ -17,14 +17,35 @@ execuçao:
     ou
     ./hello.py
 """
-__version__= "0.1.0"
+__version__= "0.1.3"
 __author__= "Pedro Henrique"
 __license__= "unlicense"
 
 import os
 
-current_lenguage = os.getenv("LANG", "en_US")[:5]
+import sys
 
+arguments = {"lang": None, "count": 1}
+
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value
+    
+current_language = arguments["lang"]
+
+if current_language is None:
+    # TODO: usar repetiçao        
+    env_lang = os.getenv("LANG","").strip()
+    if env_lang:
+        current_language = env_lang[:5]
+    else:
+        current_language = input("Choose a language (ex: en_US, pt_BR):")
+        
 msg = {
     "pt_BR": "Ola, mundo!",
     "en_US": "Hello world!",
@@ -33,4 +54,4 @@ msg = {
     "ar_SA": "مرحبا بالعالم",
 }
 
-print(msg[current_lenguage])
+print(msg[current_language] * int(arguments["count"]))
